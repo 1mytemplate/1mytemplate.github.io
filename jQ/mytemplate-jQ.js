b32f9d2204c261b72c9cc801affa27b68f4dd62f
1mytemplate <37050478+1mytemplate@users.noreply.github.com>
2018-03-04 23:57:18 +0300

    Add files via upload

---
+++ jQ/mytemplate-jQ.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){	
+	$(function() {
+	  $(window).scroll(function() {
+	  if($(this).scrollTop() != 0) {
+	  $('.top_button').fadeIn();
+	  } else {
+	  $('.top_button').fadeOut();
+	  }
+	  });
+	  $('.top_button').click(function() {
+	  $('body,html').animate({scrollTop:0},700);
+	  });
+	});
+	
+		$(".tariff-order").click(function(){
+        $(".form").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+		$(".order").click(function(){
+        $(".form").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+
+});
